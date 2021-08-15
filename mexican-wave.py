96923c9ffd5bc402e8ec79fbf01ccd2eb7ca7f18
PranayRoberts <86553739+PranayRoberts@users.noreply.github.com>
2021-08-15 16:36:02 -0700

    Create mexican-wave.py

---
+++ mexican-wave.py
@@ -0,0 +1,12 @@
+def wave(people):
+    l=len(people)
+    a=[]
+    for i in range(l):
+        s=""
+        if people[i]==" ":
+           continue
+        else:
+            s= people[:i] + people[i].upper() + people[i+1:l]
+            a.append(s)
+        
+    return a
